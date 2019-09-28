0a0b4d3e60906285f5ef8f477919591f76697af2
Eric H77 <rico@club-internet.fr>
2019-09-28 18:05:43 +0200

    Update MenuJourTableau.js

--- src/components/globals/MenuJourTableau.js
+++ src/components/globals/MenuJourTableau.js
@@ -1,15 +1,30 @@
 import React from 'react'
+import Hero from './Hero'
 
+import { useStaticQuery, graphql } from 'gatsby'
+
+const getDefaultImg = graphql`
+query {
+  file(relativePath: {eq: "menu-bkg.jpg"}){
+    childImageSharp {
+      fluid(quality: 90, maxWidth: 900){
+        ...GatsbyImageSharpFluid
+      }
+    }
+  }
+}`
 
 const MenuJourTableau = ({day, plats}) => {
-  
+  const data = useStaticQuery(getDefaultImg)
   return (
+    
     <section name="menu-jour-tableau">
-
-      <div className="menu-jour-tableau-container">
-        <h1>Au menu ce {day}</h1>
-        { plats.map(item => <h2 key={item.node.id}>- {item.node.title}</h2>) }
-      </div>
+      <Hero className="menu-background" img={data.file.childImageSharp.fluid}>
+        <div className="menu-jour-tableau-container">
+          <h1>Au menu ce {day}</h1>
+          { plats.map(item => <h2 key={item.node.id}>- {item.node.title}</h2>) }
+        </div>    
+      </Hero>
     </section>
   )
 }
